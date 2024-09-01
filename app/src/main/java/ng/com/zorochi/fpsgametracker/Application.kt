b4965155087ecaac0628f2367277087e297044ac
Zoro <jagbetuyi001@gmail.com>
2024-09-01 16:40:24 +0100

    added global error handling

---
+++ app/src/main/java/ng/com/zorochi/fpsgametracker/Application.kt
@@ -0,0 +1,21 @@
+package ng.com.zorochi.fpsgametracker
+
+import android.app.Application
+import android.util.Log
+import kotlin.system.exitProcess
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Set default uncaught exception handler
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e("UncaughtException", "Uncaught exception in thread ${thread.name}", throwable)
+            // Perform any necessary cleanup here
+
+            // Close the app gracefully
+            exitProcess(2)
+        }
+    }
+}
